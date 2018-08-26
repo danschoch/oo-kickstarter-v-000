6c75e65ac8bae2bf27b4ef54613c5f3e6ff08df5
Daniel Schoch <d.schoch90@gmail.com>
2018-08-26 20:37:36 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,25 +8,8 @@ class Project
     @backers = []
   end
 
-  def self.all
-    @@all_projects
-  end
-
-  def self.create(title)
-    new_project = self.new(title)
-    @@all_projects << new_project
-  end
-
-  def self.find(title)
-    self.all.find {|project| project.title == title}
-  end
-
   def add_backer(backer)
     @backers << backer
     backer.backed_projects << self
-  end
-
-  def self.find_or_create_by_title(title)
-    self.find(title) ? self.find(title) : self.create(title)
   end
 end
