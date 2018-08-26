3d90c3c100d7a32372b58cb695642b66aa35516f
Daniel Schoch <d.schoch90@gmail.com>
2018-08-26 20:29:37 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,24 @@
 class Project
   attr_accessor :title, :backers
 
+  @@all_projects =[]
+
   def initialize(title)
     @title = title
     @backers = []
   end
 
+  def self.create(title)
+    new_project = self.new(title)
+    @@all_projects << new_project
+  end
+
   def add_backer(backer)
     @backers << backer
+  end
+
+  def find_or_create_by_title(title)
+    self.find(title) ? self.find(title) : self.create(title)
+
   end
 end
